70ae4719a17fff2fd9e77f2a1b264d86b269eba5
Eric Soppi <soppi.2@osu.edu>
2021-11-17 13:09:03 -0800

    Use printer constants in ExpressionPrinterRecursive

--- src/main/java/compiler/printer/impl/ExpressionPrinterRecursive.kt
+++ src/main/java/compiler/printer/impl/ExpressionPrinterRecursive.kt
@@ -1,40 +1,41 @@
 package compiler.printer.impl
 
 import compiler.core.*
+import compiler.core.constants.PrinterConstants
 import compiler.printer.impl.internal.IExpressionPrinter
 
 class ExpressionPrinterRecursive : IExpressionPrinter {
     override fun printParsedNode(node: IParsedExpressionNode): String {
         return when(node) {
             is ParsedBinaryArrayOperatorNode -> {
-                printParsedNode(node.variableExpression) + "[" + printParsedNode(node.insideExpression) + "]"
+                printParsedNode(node.variableExpression) + PrinterConstants.LEFT_BRACKET + printParsedNode(node.insideExpression) + PrinterConstants.RIGHT_BRACKET
             }
             is ParsedBinaryAssignNode -> {
-                printParsedNode(node.leftExpression) + " = " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + PrinterConstants.EQUALS + PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedBinaryAssignOperatorNode -> {
-                printParsedNode(node.leftExpression) + " " + node.operator + "= " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + node.operator + PrinterConstants.EQUALS + PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedBinaryOperatorNode -> {
-                printParsedNode(node.leftExpression) + " " + node.operator +  " " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + node.operator +  PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedBinaryAndOperatorNode -> {
-                printParsedNode(node.leftExpression) + " && " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + PrinterConstants.AMPERSAND + PrinterConstants.AMPERSAND + PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedBinaryOrOperatorNode -> {
-                printParsedNode(node.leftExpression) + " || " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + PrinterConstants.VERTICAL_BAR + PrinterConstants.VERTICAL_BAR + PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedBinaryRelationalOperatorNode -> {
-                printParsedNode(node.leftExpression) + " " + node.operator + " " + printParsedNode(node.rightExpression)
+                printParsedNode(node.leftExpression) + PrinterConstants.SPACE + node.operator + PrinterConstants.SPACE + printParsedNode(node.rightExpression)
             }
             is ParsedInnerExpression -> {
-                "(" + printParsedNode(node.expression) + ")"
+                PrinterConstants.LEFT_PARENTHESES + printParsedNode(node.expression) + PrinterConstants.RIGHT_PARENTHESES
             }
             is ParsedUnaryOperatorNode -> {
                 node.operator + printParsedNode(node.expression)
             }
             is ParsedUnaryNotOperatorNode -> {
-                "!" + printParsedNode(node.expression)
+                PrinterConstants.EXCLAMATION_POINT + printParsedNode(node.expression)
             }
             is ParsedUnaryPreOperatorNode -> {
                 node.operator + node.operator + printParsedNode(node.expression)
